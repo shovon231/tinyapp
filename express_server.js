b21e0a90deea6a1ae88d97793dfb050c0e26e982
Mohammad Razaul Islam <shovon231@gmail.com>
2023-03-18 01:33:26 +0000

    adding the post method and create function for random string

--- express_server.js
+++ express_server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+let generateRandomString = function () {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 1; i <= 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -16,8 +26,13 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
@@ -27,5 +42,5 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on ports ${PORT}!`);
 });
